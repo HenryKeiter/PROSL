72be8c87d7388ade336f4ad81bbef467eda2e042
Henry Keiter <henry.keiter@gmail.com>
2013-05-10 11:54:15 -0600

    Split some functionality out into _resources.

--- prosl.py
+++ prosl.py
@@ -5,163 +5,32 @@ more difficult to read: sentences with too many words, sentences with too many
 characters, and words repeated in close proximity. All parameters can be 
 configured at runtime.
 
-Run prose_validator.py -h for detailed options.
+Run with -h for detailed options.
 
 Created on Nov 26, 2012
 
-@author: DrBwaa
+@author: Henry Keiter
+@version: Python 3.3
 
-@TODO: Publish this version (github maybe?)
+@TODO: Publish
 @TODO: use string.punctuation!!
 '''
 
 import collections
 import os
 import sys
-
-_COMMON_WORDS = ["","a","about","all","an","and","are","as","at",
-    "be","been","but","by","call","can","come","could","day","did",
-    "do","down","each","find","first","for","from","go","had","has",
-    "have","he","her","him","his","hot","how","i","if","in","is","it",
-    "know","like","long","look","make","many","may","more","most",
-    "my","no","now","number","of","on","one","or","other","out",
-    "over","people","said","see","she","side","so","some","sound",
-    "than","that","the","their","them","then","there","these","they",
-    "thing","this","time","to","two","up","use","was","water","way",
-    "we","were","what","when","which","who","will","with","word",
-    "would","write","you","your"]
-
-_COMMON_WORDS_EXTENSION = ["able","above","act","add","afraid","after",
-    "again","against","age","ago","agree","air","allow","also",
-    "always","am","among","anger","animal","answer","any","appear",
-    "apple","area","arm","arrange","arrive","art","ask","atom",
-    "baby","back","bad","ball","band","bank","bar","base","basic",
-    "bat","bear","beat","beauty","bed","before","began","begin",
-    "behind","believe","bell","best","better","between","big","bird",
-    "bit","black","block","blood","blow","blue","board","boat",
-    "body","bone","book","born","both","bottom","bought","box","boy",
-    "branch","bread","break","bright","bring","broad","broke",
-    "brother","brought","brown","build","burn","busy","buy","came",
-    "camp","capital","captain","car","card","care","carry","case",
-    "cat","catch","caught","cause","cell","cent","center","century",
-    "certain","chair","chance","change","character","charge","chart",
-    "check","chick","chief","child","children","choose","chord",
-    "circle","city","claim","class","clean","clear","climb","clock",
-    "close","clothe","cloud","coast","coat","cold","collect","colony",
-    "color","column","common","company","compare","complete",
-    "condition","connect","consider","consonant","contain","continent",
-    "continue","control","cook","cool","copy","corn","corner",
-    "correct","cost","cotton","count","country","course","cover",
-    "cow","crease","create","crop","cross","crowd","cry","current",
-    "cut","dad","dance","danger","dark","dead","deal","dear","death",
-    "decide","decimal","deep","degree","depend","describe","desert",
-    "design","determine","develop","dictionary","die","differ",
-    "difficult","direct","discuss","distant","divide","division",
-    "doctor","does","dog","dollar","don't","done","door","double",
-    "draw","dream","dress","drink","drive","drop","dry","duck",
-    "during","ear","early","earth","ease","east","eat","edge",
-    "effect","egg","eight","either","electric","element","else","end",
-    "enemy","energy","engine","enough","enter","equal","equate",
-    "especially","even","evening","event","ever","every","exact",
-    "example","except","excite","exercise","expect","experience",
-    "experiment","eye","face","fact","fair","fall","family","famous",
-    "far","farm","fast","fat","father","favor","fear","feed","feel",
-    "feet","fell","felt","few","field","fig","fight","figure","fill",
-    "final","fine","finger","finish","fire","fish","fit","five",
-    "flat","floor","flow","flower","fly","follow","food","foot",
-    "force","forest","form","forward","found","four","fraction",
-    "free","fresh","friend","front","fruit","full","fun","game",
-    "garden","gas","gather","gave","general","gentle","get","girl",
-    "give","glad","glass","gold","gone","good","got","govern",
-    "grand","grass","gray","great","green","grew","ground","group",
-    "grow","guess","guide","gun","hair","half","hand","happen",
-    "happy","hard","hat","head","hear","heard","heart","heat",
-    "heavy","held","help","here","high","hill","history","hit",
-    "hold","hole","home","hope","horse","hour","house","huge",
-    "human","hundred","hunt","hurry","ice","idea","imagine","inch",
-    "include","indicate","industry","insect","instant","instrument",
-    "interest","invent","iron","island","job","join","joy","jump",
-    "just","keep","kept","key","kill","kind","king","knew","lady",
-    "lake","land","language","large","last","late","laugh","law",
-    "lay","lead","learn","least","leave","led","left","leg","length",
-    "less","let","letter","level","lie","life","lift","light","line",
-    "liquid","list","listen","little","live","locate","log","lone",
-    "lost","lot","loud","love","low","machine","made","magnet",
-    "main","major","man","map","mark","market","mass","master",
-    "match","material","matter","me","mean","meant","measure","meat",
-    "meet","melody","men","metal","method","middle","might","mile",
-    "milk","million","mind","mine","minute","miss","mix","modern",
-    "molecule","moment","money","month","moon","morning","mother",
-    "motion","mount","mountain","mouth","move","much","multiply",
-    "music","must","name","nation","natural","nature","near",
-    "necessary","neck","need","neighbor","never","new","next","night",
-    "nine","noise","noon","nor","north","nose","note","nothing",
-    "notice","noun","numeral","object","observe","occur","ocean",
-    "off","offer","office","often","oh","oil","old","once","only",
-    "open","operate","opposite","order","organ","original","our",
-    "own","oxygen","page","paint","pair","paper","paragraph","parent",
-    "part","particular","party","pass","past","path","pattern","pay",
-    "perhaps","period","person","phrase","pick","picture","piece",
-    "pitch","place","plain","plan","plane","planet","plant","play",
-    "please","plural","poem","point","poor","populate","port","pose",
-    "position","possible","post","pound","power","practice","prepare",
-    "present","press","pretty","print","probable","problem","process",
-    "produce","product","proper","property","protect","prove",
-    "provide","pull","push","put","quart","question","quick","quiet",
-    "quite","quotient","race","radio","rail","rain","raise","ran",
-    "range","rather","reach","read","ready","real","reason","receive",
-    "record","red","region","remember","repeat","reply","represent",
-    "require","rest","result","rich","ride","right","ring","rise",
-    "river","road","rock","roll","room","root","rope","rose","round",
-    "row","rub","rule","run","safe","sail","salt","same","sand",
-    "sat","save","saw","say","scale","school","science","score",
-    "sea","search","season","seat","second","section","seed","seem",
-    "segment","select","self","sell","send","sense","sent","sentence",
-    "separate","serve","set","settle","seven","several","shall",
-    "shape","share","sharp","sheet","shell","shine","ship","shoe",
-    "shop","shore","short","should","shoulder","shout","show","sight",
-    "sign","silent","silver","similar","simple","since","sing",
-    "single","sister","sit","six","size","skill","skin","sky",
-    "slave","sleep","slip","slow","small","smell","smile","snow",
-    "soft","soil","soldier","solution","solve","son","song","soon",
-    "south","space","speak","special","speech","speed","spell",
-    "spend","spoke","spot","spread","spring","square","stand","star",
-    "start","state","station","stay","stead","steam","steel","step",
-    "stick","still","stone","stood","stop","store","story","straight",
-    "strange","stream","street","stretch","string","strong","student",
-    "study","subject","substance","subtract","success","such","sudden",
-    "suffix","sugar","suggest","suit","summer","sun","supply",
-    "support","sure","surface","surprise","swim","syllable","symbol",
-    "system","table","tail","take","talk","tall","teach","team",
-    "teeth","tell","temperature","ten","term","test","thank","thick",
-    "thin","think","third","those","though","thought","thousand",
-    "three","through","throw","thus","tie","tiny","tire","together",
-    "told","tone","too","took","tool","top","total","touch","toward",
-    "town","track","trade","train","travel","tree","triangle","trip",
-    "trouble","truck","true","try","tube","turn","twenty","type",
-    "under","unit","until","us","usual","valley","value","vary",
-    "verb","very","view","village","visit","voice","vowel","wait",
-    "walk","wall","want","war","warm","wash","watch","wave","wear",
-    "weather","week","weight","well","went","west","wheel","where",
-    "whether","while","white","whole","whose","why","wide","wife",
-    "wild","win","wind","window","wing","winter","wire","wish",
-    "woman","women","won't","wonder","wood","work","world","written",
-    "wrong","wrote","yard","year","yellow","yes","yet","young"]
+from _resources improt NWS_DELIMITERS, TERMINATORS, NON_TERMINATORS, PUNCTUATION
 
 def _common_words(**opts):
-    if opts.get('extended_list'):
-        w = [x for x in _COMMON_WORDS + _COMMON_WORDS_EXTENSION]
-        w.sort()
-        return w
     if opts.get('track_all_words'):
         return ['']
-    return [x for x in _COMMON_WORDS]
-
-_NWS_DELIMITERS = ['--','-','\x97']
-_PUNCTUATION = ''.join(['.',',','?','!',"'",'"',':',';','(',')','/','`','-',
-                       '\x92','\x93','\x94','\x97'])
-_TERMINATORS = ['.', '?', '!']
-_NON_TERMINATORS = ['Dr.', 'Ms.', 'Mrs.', 'Mr.', 'Mme.', 'Jr.', 'Sr.']
+    from _resources import COMMON_WORDS
+    if opts.get('extended_list'):
+        from _resources improt COMMON_WORDS_EXTENSION
+        w = [x for x in COMMON_WORDS + COMMON_WORDS_EXTENSION]
+        w.sort()
+        return w
+    return [x for x in COMMON_WORDS]
 
 def _search(a, x):
     '''Quick & dirty binary search for the common word list.'''
@@ -210,7 +79,7 @@ def _option_parser():
 
 def _split_text(text):
     tokens = text.split()
-    for d in _NWS_DELIMITERS:
+    for d in NWS_DELIMITERS:
         tokens = reduce(lambda a,b:a+b,map(lambda x:x.split(d),tokens),[])
     return tokens
 
@@ -289,8 +158,8 @@ def _get_stats(text):
     frequency = {}
     for token in split:
         current_sen.append(token)
-        if any(t in token for t in _TERMINATORS):
-            if not any(t in token for t in _NON_TERMINATORS):
+        if any(t in token for t in TERMINATORS):
+            if not any(t in token for t in NON_TERMINATORS):
                 sentences.append(' '.join(current_sen))
                 current_sen = []
         
@@ -298,7 +167,7 @@ def _get_stats(text):
         # Note that if augmented forms appear before basic ones, they'll both
         # be caught separately. Consider using a wordlist to improve this, e.g.
         # http://www.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt
-        stoken = token.strip(_PUNCTUATION).lower()
+        stoken = token.strip(PUNCTUATION).lower()
         alnum_count += len(stoken)
         if stoken in frequency:
             frequency[stoken] += 1
@@ -341,7 +210,7 @@ def validate(text, **opts):
     current_sentence = []
     
     for token in _split_text(text):
-        simpletoken = token.strip(_PUNCTUATION).lower()
+        simpletoken = token.strip(PUNCTUATION).lower()
         if proximity:
             last_n_tokens.append(token)
             try:
@@ -352,8 +221,8 @@ def validate(text, **opts):
                                     ' '.join(last_n_tokens)))
             last_n_simple_tokens.append(simpletoken)
         current_sentence.append(token)
-        if any(t in token for t in _TERMINATORS):
-            if not any(t in token for t in _NON_TERMINATORS):
+        if any(t in token for t in TERMINATORS):
+            if not any(t in token for t in NON_TERMINATORS):
                 # End of sentence; check for problems.
                 if wthresh and len(current_sentence) >= wthresh:
                     phrases.append(('Word-count threshold exceeded', 
@@ -412,7 +281,7 @@ def main():
         with open(os.path.abspath(args[1]), 'rb') as f:
             text = f.read()
     except:
-        print 'Unable to open file "%s".'
+        print('Unable to open file "%s".')
         return
     
     flags, stats = validate(text, **opts.__dict__)
@@ -426,13 +295,13 @@ def main():
                     f.write('\n\n### Stats ###\n\n')
                     f.write(_format_stats(stats))
         except:
-            print 'Error writing flags to file.'
+            print('Error writing flags to file.')
     else:
-        print '\n'.join(map(str,flags))
-        print 'Total number of flags:\t%s' % len(flags)
+        print('\n'.join(map(str,flags)))
+        print('Total number of flags:\t%s' % len(flags))
         if stats:
-            print ('\n\n### Stats ###\n\n')
-            print _format_stats(stats)
+            print('\n\n### Stats ###\n\n')
+            print(_format_stats(stats))
 
 if __name__ == '__main__':
     main()
